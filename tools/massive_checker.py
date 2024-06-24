ffe8cc3688fd90ff018a5c8f98ef7cdc5fe09d1a
3v401 <alvarohuanay7@gmail.com>
2024-06-24 10:25:52 +0200

    Update massive_checker.py

--- tools/massive_checker.py
+++ tools/massive_checker.py
@@ -1,3 +1,5 @@
 """
 This program checks in your filesystem what folder is the most heavy in your filesystem and asks you whether to clean it or not
+
+This program analyzes the files at the path ~HOME which is the disk with less space
 """
