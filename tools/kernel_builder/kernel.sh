722e006263ada5bb9491550c1b866a043f82b0ee
3v401 <alvarohuanay7@gmail.com>
2024-07-11 11:15:16 +0200

    Update kernel.sh

--- tools/kernel_builder/kernel.sh
+++ tools/kernel_builder/kernel.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+echo '#!/bin/bash'"
 
-# These loaded modules correspond to HPC systems (center not mentioned) version 2024.
+# Note: My outcome after creating kernel.sh won't be shared because of privacy/security reasons. 
 
-# export PROJECT=Type_your_project # Project you are working with
 
-ml --force purge
-jutil env activate -p $PROJECT
+module purge
+# cslqip is my project, use yours.
+jutil env activate -p $PROJECT_NAME
 cd $PROJECT
 export USERINSTALLATIONS=${PROJECT}/${USER}
 cd ${PROJECT}/${USER}
 ml --force purge
 ml Stages/2024 UserInstallations
-module load GCCcore/.12.3.0
-module load DWave/6.8.0
+module load ${MODULE_LIST}
 
-    
 # Activate your Python virtual environment
 source ${KERNEL_VENVS_DIR}/${KERNEL_NAME}/bin/activate
-    
+
 # Ensure python packages installed in the virtual environment are always prefered
-export PYTHONPATH=${VIRTUAL_ENV}/lib/python3.11/site-packages:${PYTHONPATH}
-    
-exec python -m ipykernel $@
+export PYTHONPATH=${VIRTUAL_ENV}/lib/python3.11/site-packages:"'${PYTHONPATH}'"
+
+exec python -m ipykernel "'$@' > ${VIRTUAL_ENV}/kernel.sh
+chmod +x ${VIRTUAL_ENV}/kernel.sh
