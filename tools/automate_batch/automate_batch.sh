9774ae5f294545c43a4368e2852d5e93c166bd7c
3v401 <alvarohuanay7@gmail.com>
2024-08-07 11:26:56 +0200

    Update automate_batch.sh

--- tools/automate_batch/automate_batch.sh
+++ tools/automate_batch/automate_batch.sh
@@ -1,1 +1,14 @@
-It takes as input the script to run and tests this script on every machine (jureca, juwels, jusuf, --)
+It takes as input the script to run and tests this script on every machine (jureca, juwels, jusuf, judac)
+
+To use each of them you must be a member of the project:
+
+hdfml: cswmanage (no batch system found)
+judac: cswmanage, jqctst01, qlmtst01, cslqip (no batch system found)
+jureca: cswmanage (done)
+jusuf: cswmanage (done)
+juwels: cswmanage (done)
+juwels_booster: cswmanage (done)
+wsgjsc: wsgjsctst (no batch system found)
+juqcs: jqctst01 (no batch system found)
+
+You loggin to each system with ssh -4. Run the .sh script with --user-account = cswmanage
