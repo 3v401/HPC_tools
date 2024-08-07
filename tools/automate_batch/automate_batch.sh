cba0915f60b05765aaf32cbf5d72e68f3e302dcc
3v401 <alvarohuanay7@gmail.com>
2024-08-07 11:31:52 +0200

    Update automate_batch.sh

--- tools/automate_batch/automate_batch.sh
+++ tools/automate_batch/automate_batch.sh
@@ -11,6 +11,9 @@ juwels_booster: cswmanage (done)
 wsgjsc: wsgjsctst (no batch system found)
 juqcs: jqctst01 (no batch system found)
 
+Therefore the systems we will test are: jureca, jusuf, juwels, juwels-booster
+User is always: cswmanage
+
 You loggin to each system with ssh -4. Run the .sh script with --user-account = cswmanage
 
 Partitions:
@@ -18,3 +21,12 @@ jureca: dc-cpu-devel, dc-gpu-devel
 juwels-cluster: devel, develgpus
 juwels-booster: develbooster
 jusuf: batch, develgpus
+
+The script is run on each system. So it will run:
+
+cpu job
+cpu distributed job
+gpu job
+gpu distributed job
+
+And return the outcomes.
